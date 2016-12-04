91fe84893c21ee7c2573f1e312facab5f9aa5b60
Louis Mau <lmau404@mtroyal.ca>
2016-12-03 16:19:47 -0800

    Added Default layout

--- app/src/main/java/com/example/user/addedme11/MainActivity.java
+++ app/src/main/java/com/example/user/addedme11/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.user.addedme11;
 
+import android.net.Uri;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void sendFacebook(View view){
+
+        Uri uri = Uri.parse("https://facebook.com");
+        Inten
+
+
+
+
+    }
+
+
 }
