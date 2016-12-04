719255e9321459cca354ef11186be3e6bec5c605
Louis Mau <lmau404@mtroyal.ca>
2016-12-03 16:52:06 -0800

    some stuff added

--- app/src/main/java/com/example/user/addedme11/MainActivity.java
+++ app/src/main/java/com/example/user/addedme11/MainActivity.java
@@ -1,25 +1,31 @@
 package com.example.user.addedme11;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private WebView wv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        wv = new WebView(this);
+        //setContentView(wv);
     }
 
     public void sendFacebook(View view){
 
         Uri uri = Uri.parse("https://facebook.com");
-        Inten
-
-
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+        System.out.println(uri.toString());
+        wv.loadUrl(uri.toString());
 
 
     }
